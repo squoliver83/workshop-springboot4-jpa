ce04fc4072396dbcca7000b0d279621d26109eff
Samuel Quaresma <squoliver@gmail.com>
2026-06-29 10:16:15 -0300

    Projetct created

---
+++ src/test/java/com/javacompleto/udemycourse/UdemycourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacompleto.udemycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemycourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
